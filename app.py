ff91d2e2b7df6a53550bc91c4b2563d0f1915825
josippozega <96910194+josippozega@users.noreply.github.com>
2022-01-05 08:05:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ if city_name:
             df['Max temp'] = max_temp_df
             st.table(df)
 
-	st.map(pd.DataFrame({'lat' : [res[5]] , 'lon' : [res[4]]},columns = ['lat','lon']))
+	#st.map(pd.DataFrame({'lat' : [res[5]] , 'lon' : [res[4]]},columns = ['lat','lon']))
 
 
 
